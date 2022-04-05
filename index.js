dbb2ab74858fa387a4da4d38b2376a25b7f8251a
abubaker <abubakerdahab@gmail.com>
2022-04-06 00:32:24 +0200

    add auth routes

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 const projects = require("../routes/projects");
 const Bugs = require("../routes/bugs");
 const users = require("../routes/users");
 
 const app = express();
 app.use(express.json());
+dotenv.config();
 
 app.use("/api/v1/users", users);
 app.use("/api/v1/projects", projects);
 app.use("/api/v1/bugs", Bugs);
 
-const MONGO_URL = "mongodb://localhost:27017/bug-track-db";
-const PORT = 5000;
+const MONGO_URL = process.env.MONGO_URL;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(MONGO_URL, {
--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const mongoose = require("mongoose");
 
@@ -42,9 +44,42 @@ function validateUser(user) {
   return schema.validate(user);
 }
 
+function validateRegister(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(4).max(10).required(),
+    email: Joi.string().min(8).max(255).required().email(),
+    password: Joi.string().min(6).max(255).required(),
+  });
+
+  return schema.validate(user);
+}
+
+function validateLogin(user) {
+  const schema = Joi.object({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6).max(255),
+  });
+  return schema.validate(user);
+}
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+      isAdmin: this.isAdmin,
+    },
+    process.env.JWT_SECRIT
+  );
+  return token;
+};
+
 userSchema.methods.matchPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
 module.exports.User = mongoose.model("User", userSchema);
 module.exports.validateUser = validateUser;
+module.exports.validateRegister = validateRegister;
+module.exports.validateLogin = validateLogin;
